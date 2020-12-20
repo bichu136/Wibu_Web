56964220abf720fc3884fa8493a95ab57a478135
bichu136 <bicagu123@gmail.com>
2020-12-20 16:05:42 +0700

    them viec cho Dat

--- app.py
+++ app.py
@@ -12,6 +12,15 @@ def index():
 @app.route("/AboutMe")
 def aboutme():
     return flk.render_template("Aboutme.html")
+@app.route("/film/<film_name>")
+def anime_film(film_name):
+    print(film_name)
+    return flk.render_template("1anime.html",film_name=film_name)
+@app.route("/category/<category_name>")
+def anime_category(category_name):
+    print(category_name)
+    return flk.render_template("1cat.html",cat_name=category_name)
+
 
 if __name__ == '__main__':
     app.run(debug=True,port=int(os.environ.get('PORT', 8080)))
