7db9536cfdd3fb8c2219982acee8153f8c519c42
bichu136 <bicagu123@gmail.com>
2020-12-26 23:00:57 +0700

    clean data almost

--- app.py
+++ app.py
@@ -10,10 +10,21 @@ NEW_DOMAIN = "https://cdn.myanimelist.net/"
 # import keras.layers as layers
 # import keras.models as models
 data = pd.read_csv('./datasets/AnimeList.csv')
+
+
 # data preprocessing
+
+#remove date Not available
+data1 = data[data['aired_string']!='Not available']
+# remove film that not airing 
+data2 = data1[data1['aired']!="{'from': None, 'to': None}"]
+# remove score <=3
+data3 = data2[data2['score']>=3]
+# remove favorites =0
+data4 = data3[data3['favorites']!=0]
+data4['rank'].fillna(0);
+data5 = data4[data4['rank']>0]
 # change domain 
-# for t in data['image_url'].values:
-#      print(type(t))
 def new_domain(str):
     r = ""
     if type(str)==type("str"):
@@ -21,14 +32,22 @@ def new_domain(str):
         r = NEW_DOMAIN +t[-4] +'/'+t[-3] +'/'+t[-2] +'/'+t[-1]
     return r
 new_img_url = pd.DataFrame({'image_url': [new_domain(t) for t in data['image_url'].values]})
-data.update(new_img_url)
+data5.update(new_img_url)
 
+
+
+# aired_string process
+date_time_aired = pd.DataFrame({'aired_str':[i.split('to')[0] for i in data5['aired_string']]})
+data5.insert(0,"aired_str",pd.to_datetime(date_time_aired['aired_str']).to_list(),True)
+#sort value
+data5 = data5.sort_values(by='aired_str',ascending=False)
+print(data5[['aired_str','aired_string']])
 app = flk.Flask(__name__)
-url_D = data[["image_url","title"]]
-print(url_D[url_D.columns].iloc[0:4])
+url_D = data5[["image_url","title"]]
 @app.route("/")
 def index():
-    print(data)
+    print(url_D['image_url'])
+    print(url_D['title'])
     num_row= 6
     
     rows = [[[url_D['title'].iloc[j+i*4],url_D['image_url'].iloc[j+i*4]] for j in range(4)] for i in range(num_row)]
