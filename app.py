1fc53dc297ab123d1f7a712fabaf0d3e399efb73
bichu136 <bicagu123@gmail.com>
2021-01-09 20:52:37 +0700

    mapping add

--- app.py
+++ app.py
@@ -9,6 +9,19 @@ indexer = TfIdfIndex()
 # import keras
 # import keras.layers as layers
 # import keras.models as models
+def get_genres(x):
+    return []
+def get_rows(url_D,num_row,num_col,p):
+    return [
+        [
+        [
+        url_D['title'].iloc[num_row*num_col*(p-1)+j+i*num_col],
+        url_D['image_url'].iloc[num_row*num_col*(p-1)+j+i*num_col]
+        ] 
+        for j in range(num_col) if num_row*num_col*(p-1)+j+i*num_col < len(url_D)
+        ] 
+        for i in range(num_row) 
+        ]
 data = pd.read_csv('processed_data.csv')
 mapper = open('mapping_category.txt').readlines()
 mapper = {i.strip().split('|')[0]: i.strip().split('|')[1] for i in mapper}
@@ -23,7 +36,7 @@ def index():
     url_D = data[["image_url","title"]]
     num_row= 6
     num_col=4
-    rows = [[[url_D['title'].iloc[num_row*num_col*(1-1)+j+i*num_col],url_D['image_url'].iloc[num_row*num_col*(1-1)+j+i*num_col]] for j in range(num_col)] for i in range(num_row)]
+    rows = get_rows(url_D,num_row,num_col,p=1)
     return flk.render_template("index.html",rows=rows)
 @app.route("/AboutMe")
 def aboutme():
@@ -31,7 +44,12 @@ def aboutme():
 @app.route("/film/<film_name>")
 def anime_film(film_name):
     print(film_name)
-    return flk.render_template("anime-info.html",film_name=film_name)
+    x = data[data['title']==film_name].iloc[0]
+    title =  x['title']
+    genres = get_genres(x)
+    episodes = x['episodes']
+    content = x['content']
+    return flk.render_template("anime-info.html",film_name=title)
 @app.route("/category/<category_name>/<page>")
 def anime_category(category_name,page):
     # sort to specific category
@@ -42,7 +60,7 @@ def anime_category(category_name,page):
     url_D = data8[["image_url","title"]]
     print(data8)
     print(url_D)
-    rows = [[[url_D['title'].iloc[num_row*num_col*(p-1)+j+i*num_col],url_D['image_url'].iloc[num_row*num_col*(p-1)+j+i*num_col]] for j in range(num_col)] for i in range(num_row)]
+    rows = get_rows(url_D,num_row,num_col,p)
     return flk.render_template("category_list.html",rows = rows, category_name=category_name)
 
 @app.route("/list/<page>")
@@ -51,7 +69,7 @@ def anime_page(page):
     num_col=4
     p = int(page)
     url_D = data[["image_url","title"]]
-    rows = [[[url_D['title'].iloc[num_row*num_col*(p-1)+j+i*num_col],url_D['image_url'].iloc[num_row*num_col*(p-1)+j+i*num_col]] for j in range(num_col)] for i in range(num_row)]
+    rows = get_rows(url_D,num_row,num_col,p)
     return flk.render_template("category_list.html",page=page,rows=rows)
 @app.route("/year/<year>")
 def anime_year(year):
@@ -60,7 +78,7 @@ def anime_year(year):
 @app.route("/api/get_searched_list",methods=['post'])
 def get_search_list():
     query = request.form['query']
-    return flk.url_for('.searched',query=query,page='1')
+    return flk.url_for('searched',query=query,page='1')
 @app.route("/searched",methods=["post","get"])
 def searched():
     query = request.args['query']
@@ -80,19 +98,11 @@ def searched():
     print(x.columns)
     
     url_D = x[["image_url","title"]]
-    rows = [
-        [
-        [
-        url_D['title'].iloc[num_row*num_col*(p-1)+j+i*num_col],
-        url_D['image_url'].iloc[num_row*num_col*(p-1)+j+i*num_col]
-        ] 
-        for j in range(num_col) if num_row*num_col*(p-1)+j+i*num_col < len(url_D)
-        ] 
-        for i in range(num_row) 
-        ]
+    rows = get_rows(url_D,num_row,num_col,p)
     return flk.render_template('category_list.html',page=p,rows=rows)
 if __name__ == '__main__':
     app.run(debug=True,port=int(os.environ.get('PORT', 8080)))
+    
     # app.run()
 # host='0.0.0.0'
 
